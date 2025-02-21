d6916840f4feafe973df5c9ad39410f9a7cb48bb
Jaideep <Jaideep.jaidee@gmail.com>
2025-02-21 08:48:46 +0530

    Revert "updating the latest code"
    This reverts commit 545222eb3af94eb00564f9197eff8245d1fa8385.

--- 2_llm_chat_app2.py
+++ 2_llm_chat_app2.py
@@ -6,30 +6,3 @@ from langchain_core.output_parsers import StrOutputParser
 load_dotenv()
 
 st.title("Generative AI app")
-
-llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
-
-if "messages" not in st.session_state:
-	st.session_state.messages = []
-
-messages = st.session_state.messages
-
-for m in messages:
-	with st.chat_message(m["role"]):
-		st.markdown(m["content"])
-
-prompt = st.chat_input("Ask your question")
-
-
-if prompt:
-	with st.chat_message("user"):
-		st.markdown(prompt)
-	
-	messages.append({"role": "user", "content": prompt})
-
-	chain = llm | StrOutputParser()
-	ai_ans = chain.stream(prompt)
-	
-	with st.chat_message("assistant"):
-		st.markdown(ai_ans)
-		messages.append({"role": "assistant", "content": ai_ans})

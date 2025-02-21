545222eb3af94eb00564f9197eff8245d1fa8385
Jaideep <Jaideep.jaidee@gmail.com>
2025-02-21 08:41:19 +0530

    updating the latest code

--- 1_simple_echo_app.py
+++ 1_simple_echo_app.py
@@ -1,3 +1,27 @@
 import streamlit as st
 
 st.title("Simple app")
+
+if "messages" not in st.session_state:
+	st.session_state.messages = []
+
+messages = st.session_state.messages
+
+for m in messages:
+	with st.chat_message(m["role"]):
+		st.markdown(m["content"])
+
+prompt = st.chat_input("Ask your question")
+
+
+if prompt:
+	with st.chat_message("user"):
+		st.markdown(prompt)
+	
+	messages.append({"role": "user", "content": prompt})
+	
+	ans = f"Hi you said {prompt}"
+
+	with st.chat_message("assistant"):
+		st.markdown(ans)
+		messages.append({"role": "assistant", "content": ans})
